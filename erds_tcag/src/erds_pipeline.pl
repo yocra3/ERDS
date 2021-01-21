7783a825fb63a50e2b7dcfe56e35941aac76c7ee
Carlos Ruiz <yocra3@gmail.com>
2021-01-21 09:51:36 +0100

    Remove test bam to allow running the method on a small dataset

--- erds_tcag/src/erds_pipeline.pl
+++ erds_tcag/src/erds_pipeline.pl
@@ -136,7 +136,7 @@ close(FAI) or die "Can't close $ref_fai_file";
 my $parameter_sample="$sample_dir/parameter-$samplename.pl";
 my $num_ram=19;
 
-test_bam();
+#test_bam();
 @para=($samplename, $bam_file, $variant_file, $aligned_length_thh, $ERDS_dir, $mq_thh, $exp_pct, $num_state, $ref_file, $ref_fai_file, $win_size_del, $win_size_dup, $sample_dir, $script_dir, $sd_dir, $log_dir, $rc_dir, $cnv_dir, $gcbin_dir, $samtools, $max_mq, $read_length, $min_det_size, $sd_win_size, $ins_pem, $gap_events, $low_scan_ratio, $pem_pairs, $low_pem_pairs, $rcp_rate, $clip_len, $clip_spt_pem, $clip_spt_npem, $softclip_check_thh, $large_pem, $small_pem, $CIAGR_length);
 open(PP,">$parameter_sample") or die "Can't open $parameter_sample";
 print PP "our \$parameter=\"$parameter_sample\";\n";
